95709f2b8784b896ecb00cc4b8d3a26343ded4e8
Thomas George <jorge0136@gmail.com>
2019-11-14 19:00:33 -0800

    Removes tokenizer and adds TODO comment

--- FortuneCookieModel.py
+++ FortuneCookieModel.py
@@ -21,6 +21,9 @@ import os
 
 from library.common import get_sequence_of_tokens, generate_padded_sequences, create_model, generate_text
 
+# When we go to add the embedding layer, use logic similar to the way this package does a pull of
+# it's embedding. https://github.com/minimaxir/gpt-2-simple/blob/master/README.md#usage
+# Search for "if not os.path.isfile(file_name):"
 
 # TODO: Move this to `init.py`
 fortune_cookie_csv_url = 'https://raw.githubusercontent.com/WomenInDataScience-Seattle/Machine_Learning_Projects/master/FortuneCookie/training_data/data.csv'
@@ -29,7 +32,7 @@ fortune_cookie_df = pd.read_csv(StringIO(fortune_cookie_string))
 # Extract the column of csv data that we want.
 corpus = fortune_cookie_df['Fortune Cookie Quotes']
 
-tokenizer = Tokenizer()
+
 inp_sequences, total_words = get_sequence_of_tokens(corpus)
 predictors, label, max_sequence_len = generate_padded_sequences(inp_sequences, total_words)
 model = create_model(max_sequence_len, total_words)
